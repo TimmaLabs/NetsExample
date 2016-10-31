43082845504ea8e5f0cc81bc60c977883b340fd3
Marko Raatikka <marko@timma.fi>
2016-10-31 17:09:05 +0200

    Add installer signing for the bundle

---
+++ bundle/Scripts/sign-bundle.bat
@@ -0,0 +1,15 @@
+﻿@echo off
+
+SET pfxPath=%1
+SET targetDir=%2
+SET password=%3
+SET enginePath="%targetDir%engine.exe"
+SET signtool="%PROGRAMFILES(x86)%\Windows Kits\10\bin\x64\signtool"
+SET insignia="%PROGRAMFILES(x86)%\WiX Toolset v3.10\bin\insignia"
+
+for /f %%i in ('dir /b %targetDir%*.bundle.exe') do SET bundlePath=%targetDir%%%i
+
+CALL %insignia% -v -nologo -ib %bundlePath% -o %enginePath%
+CALL %signtool% sign /v /f %pfxPath% /p %password% %enginePath%
+CALL %insignia% -v -nologo -ab %enginePath% %bundlePath% -o %bundlePath%
+CALL %signtool% sign /v /f %pfxPath% /d Timma /du https://timma.fi/company /p %password% %bundlePath%
--- setup/Scripts/sign-msi.bat
+++ setup/Scripts/sign-msi.bat
@@ -1,13 +1,13 @@
 @echo off
 
 SET pfxPath=%1
-SET sourceDir=%2
+SET targetDir=%2
 SET password=%3
 SET signtool="%PROGRAMFILES(x86)%\Windows Kits\10\bin\x64\signtool"
 SET insignia="%PROGRAMFILES(x86)%\WiX Toolset v3.10\bin\insignia"
 
-for /f %%i in ('dir /b %sourceDir%*.cab') do SET cabPath=%sourceDir%%%i
-for /f %%i in ('dir /b %sourceDir%*.msi') do SET msiPath=%sourceDir%%%i
+for /f %%i in ('dir /b %targetDir%*.cab') do SET cabPath=%targetDir%%%i
+for /f %%i in ('dir /b %targetDir%*.msi') do SET msiPath=%targetDir%%%i
 
 CALL %signtool% sign /v /f %pfxPath% /p %password% %cabPath%
 CALL %insignia% -v -nologo -im %msiPath%
