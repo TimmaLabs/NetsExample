57bc40a97d835baa2af41ecf51e56365a758ddda
Marko Raatikka <marko@timma.fi>
2016-11-11 16:09:57 +0200

    Build: Pass DEBUG flag on Debug build

--- script/build.bat
+++ script/build.bat
@@ -1,18 +1,25 @@
 @echo off
 
+setlocal enabledelayedexpansion
+
 SET conf=%1
 SET plat=%2
 SET ROOT_DIR=%cd%
 SET SCRIPT_DIR=%ROOT_DIR%\script
 SET BIN_DIR=bin
 SET OUTPUT_DIR=%BIN_DIR%\%plat%\%conf%
+SET CONSTANTS=PRODUCTION
 
 SET msbuild="%PROGRAMFILES(x86)%\MSBuild\14.0\Bin\MSBuild"
 
-%msbuild% "app\timma.csproj" /t:rebuild /p:Configuration=%conf%;Platform=%plat%;DefineConstants=PRODUCTION
+if "%conf%" == "Debug" (
+	SET CONSTANTS=%CONSTANTS%;DEBUG
+)
+
+%msbuild% "app\timma.csproj" /t:rebuild /p:Configuration=%conf%;Platform=%plat%;DefineConstants="!CONSTANTS!"
 :: x86 only for the installers
-%msbuild% "setup\setup.wixproj" /t:rebuild /p:Configuration=%conf%;Platform=x86;DefineConstants=PRODUCTION
-%msbuild% "bundle\bundle.wixproj" /t:rebuild /p:Configuration=%conf%;Platform=x86;DefineConstants=PRODUCTION
+%msbuild% "setup\setup.wixproj" /t:rebuild /p:Configuration=%conf%;Platform=x86;DefineConstants="!CONSTANTS!"
+%msbuild% "bundle\bundle.wixproj" /t:rebuild /p:Configuration=%conf%;Platform=x86;DefineConstants="!CONSTANTS!"
 
 :: sign the setup (.msi)
 call %SCRIPT_DIR%\sign-msi.bat "%SCRIPT_DIR%\Timma.pfx" %ROOT_DIR%\setup\bin\%conf%\ voittaja
