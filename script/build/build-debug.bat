b7404c29d27b1d3db80b4d607e1c8664dd9a5ccf
Marko Raatikka <marko@timma.fi>
2016-11-15 16:50:56 +0200

    Add .bat scripts for debug & release builds

---
+++ script/build/build-debug.bat
@@ -0,0 +1,8 @@
+@echo off
+
+SET ROOT_DIR=%cd%
+SET SCRIPT_DIR=%ROOT_DIR%\script
+SET build=%SCRIPT_DIR%\build\build.bat
+
+call %build% Debug x64
+call %build% Debug x86
---
+++ script/build/build.bat
@@ -0,0 +1,33 @@
+@echo off
+
+setlocal enabledelayedexpansion
+
+SET mode=%1
+SET arch=%2
+SET ROOT_DIR=%cd%
+SET SCRIPT_DIR=%ROOT_DIR%\script
+SET BIN_DIR=bin
+SET OUTPUT_DIR=%BIN_DIR%\%arch%\%mode%
+SET CONSTANTS=PRODUCTION
+
+SET msbuild="%PROGRAMFILES(x86)%\MSBuild\14.0\Bin\MSBuild"
+
+if "%mode%" == "Debug" (
+	SET CONSTANTS=%CONSTANTS%;DEBUG
+)
+
+%msbuild% "app\timma.csproj" /t:rebuild /p:Configuration=%mode%;Platform=%arch%;DefineConstants="!CONSTANTS!"
+:: x86 only for the installers
+%msbuild% "setup\setup.wixproj" /t:rebuild /p:Configuration=%mode%;Platform=x86;DefineConstants="!CONSTANTS!"
+%msbuild% "bundle\bundle.wixproj" /t:rebuild /p:Configuration=%mode%;Platform=x86;DefineConstants="!CONSTANTS!"
+
+:: sign the setup (.msi)
+call %SCRIPT_DIR%\sign-msi.bat "%SCRIPT_DIR%\Timma.pfx" %ROOT_DIR%\setup\bin\%mode%\ voittaja
+:: sign the bundle (.exe)
+call %SCRIPT_DIR%\sign-bundle.bat "%SCRIPT_DIR%\Timma.pfx" %ROOT_DIR%\bundle\bin\%mode%\ voittaja
+
+rd /S /Q %OUTPUT_DIR%
+
+xcopy %ROOT_DIR%\setup\bin\%mode%\* %OUTPUT_DIR% /F /Y /I
+xcopy %ROOT_DIR%\bundle\bin\%mode%\* %OUTPUT_DIR% /F /Y /I
+copy %SCRIPT_DIR%\Timma.cer %OUTPUT_DIR%
