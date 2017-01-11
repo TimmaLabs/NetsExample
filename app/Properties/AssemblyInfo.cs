d8ec799b17f9e8baade07b7e8fc52201b56dc081
Marko Raatikka <marko@timma.fi>
2017-01-11 13:16:54 +0200

    1.4.0.0

--- app/Properties/AssemblyInfo.cs
+++ app/Properties/AssemblyInfo.cs
@@ -51,7 +51,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.0.1")]
-[assembly: AssemblyFileVersion("1.3.0.1")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
 [assembly: Guid("FF0697D9-DCEE-42B3-8C2B-6C721226E273")]
 
